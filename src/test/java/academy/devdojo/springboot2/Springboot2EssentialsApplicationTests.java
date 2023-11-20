b1d0edbea56809813c0bc1aeaf77d4782e8c106a
Pedro Henrique <pedro.henrique59@outlook.com>
2023-11-20 08:53:11 -0300

    Criando Maven Profile para testes.

---
+++ src/test/java/academy/devdojo/springboot2/Springboot2EssentialsApplicationTests.java
@@ -0,0 +1,10 @@
+package academy.devdojo.springboot2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Springboot2EssentialsApplicationTests {
+
+
+
+}
